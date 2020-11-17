0c929e8e3e5a360738591134d15316dea0ae14a2
aayushkhanduri <aayushkhanduri@gmail.com>
2020-11-17 12:13:33 +0530

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    /* public function index()
+    {
+        return view('home');
+    } */
+    public function uploadfile()
+    {
+        return view('uploadfile');
+    }
+
+    /** Example of File Upload */
+    public function uploadFilePost(Request $request){
+        $request->validate([
+            'fileToUpload' => 'required|file|max:2048',
+        ]);
+
+        $fileName = "fileName".time().'.'.request()->fileToUpload->getClientOriginalExtension();
+
+        $request->fileToUpload->store('files/'.$request->user()->id,'ftp');
+        
+
+        return back()
+            ->with('success','You have successfully upload image.');
+
+    }
+
+
+    public function index(Request $request) {
+        $files = Storage::disk('ftp')->files('files/'.$request->user()->id,'ftp');
+        return view('home', ['files'=> $files]);
+        return $files;
+    }
+
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Upload File: ') }}
+                
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card">
+                <div class="card-header">Upload File Example</div>
+
+                <div class="card-body">
+                    @if ($message = Session::get('success'))
+
+                        <div class="alert alert-success alert-block">
+
+                            <button type="button" class="close" data-dismiss="alert">×</button>
+
+                            <strong>{{ $message }}</strong>
+
+                        </div>
+
+                    @endif
+
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+
+                        <form action="/uploadfile" method="post" enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <input type="file" class="form-control-file" name="fileToUpload" id="exampleInputFile" aria-describedby="fileHelp">
+                                <small id="fileHelp" class="form-text text-muted">Please upload a valid file. Size of file should not be more than 2MB.</small>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{'Uploaded Files: '}}
+
+    @foreach($files as $file)
+    <ul>
+        <li> {{ $file }}</li>
+    </ul>
+    @endforeach
+        
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
